b45b0bd1612b316588906ad7805bb75a1a5b68d6
Pat Jones <patjones80@users.noreply.github.com>
2025-11-21 14:19:37 -0800

    Create insert_flights.py

---
+++ python/insert_flights.py
@@ -0,0 +1,66 @@
+#!usr/bin/python3
+
+''' Update 9/6/2025
+
+    This code is designed to update the "flights_kXXX" tables in the adsb database by passing in 
+    bulk data loaded from a single JSON file produced by the precursor collect_history.py. This 
+    is SQLAlchemy 2.0.44 compatible.    
+'''
+from datetime import datetime
+import json
+import logging
+import time
+
+import psycopg2
+
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Session
+
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import insert
+
+from lib import get_creds
+
+# Standard connection process, replace "cnxn" as appropriate or use a credential keystore
+cred = get_creds.cred('adsb')
+cnxn = f'postgresql+psycopg2://{cred.user}:{cred.password}@{cred.host}/{cred.database}'
+
+engine = create_engine(cnxn)
+
+class Base(DeclarativeBase):
+    ''' Base class required by SQLAlchemy
+        See: https://docs.sqlalchemy.org/en/21/orm/mapping_styles.html#declarative-mapping
+    '''
+
+Base.metadata.reflect(engine)
+
+class Flights(Base):
+    ''' Establish class-based version of table "flights"
+    '''
+    __table__ = Base.metadata.tables["flights"]
+    __mapper_args__ = {"primary_key": [__table__.c.now, __table__.c.hex]}
+
+start = time.time()
+
+with open("collected_history.json", mode="r", encoding="utf-8") as history:
+    data = json.load(history)
+
+    # The dates and times are coming in as strings so convert them back
+    for ix, row in enumerate(data):
+        adsb_date, adsb_time = row['adsb_date'], row['adsb_time']
+
+        data[ix]['adsb_date'] = datetime.strptime(adsb_date, '%Y-%m-%d')
+        data[ix]['adsb_time'] = datetime.strptime(adsb_time, '%H:%M:%S')
+
+    # We take the combination of "now" and "hex" to be a compound key
+    stmt = insert(Flights).values(data)
+    stmt = stmt.on_conflict_do_nothing(index_elements=[Flights.now, Flights.hex])
+
+with Session(engine) as session:
+    try:
+        session.execute(stmt)
+        session.commit()
+    except Exception as e:
+        logging.error(f'An error occurred during bulk insert: {e}')
+
+print('Elapsed time on bulk insert: ', time.time() - start)
