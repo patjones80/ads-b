98562a2036d027c785151a924ba477af024030a9
Pat Jones <patjones80@users.noreply.github.com>
2026-06-18 16:56:51 -0700

    Clean up indentation

--- sql/get_hex.sql
+++ sql/get_hex.sql
@@ -3,10 +3,10 @@ RETURNS TEXT AS $$
 DECLARE a_hex TEXT;
 BEGIN
     SELECT hex INTO a_hex
- 	  FROM flights_kpdx 
- 	  WHERE flight = $1 AND adsb_date = $2; 
+ 	FROM flights_kpdx 
+    WHERE flight = $1 AND adsb_date = $2; 
 
- 	  RETURN a_hex;
+ 	RETURN a_hex;
 END;
 $$
  
