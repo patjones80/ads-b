3fb6a3708f94588334c73128402194e7370136b4
Pat Jones <patjones80@users.noreply.github.com>
2026-06-18 16:50:54 -0700

    Handling for inserted_at column
    The "inserted_at" column is just a custom-handled timestamp for when a particular set of records went into the table. 
    
    This update actually implements the logic for assigning a value to that column via very simple Python datetime code.

--- python/insert_flights.py
+++ python/insert_flights.py
@@ -37,22 +37,24 @@ Base.metadata.reflect(engine)
 class Flights(Base):
     ''' Establish class-based version of table "flights"
     '''
-    __table__ = Base.metadata.tables["flights"]
+    __table__ = Base.metadata.tables["flights_kpdx"]
     __mapper_args__ = {"primary_key": [__table__.c.now, __table__.c.hex]}
 
 start = time.time()
 
 with open("collected_history.json", mode="r", encoding="utf-8") as history:
     data = json.load(history)
-
-    # The dates and times are coming in as strings so convert them back
+    inserted_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    # The dates and times are coming in as strings so let's convert them back
     for ix, row in enumerate(data):
         adsb_date, adsb_time = row['adsb_date'], row['adsb_time']
+        rec = data[ix]
 
-        data[ix]['adsb_date'] = datetime.strptime(adsb_date, '%Y-%m-%d')
-        data[ix]['adsb_time'] = datetime.strptime(adsb_time, '%H:%M:%S')
+        rec['adsb_date'] = datetime.strptime(adsb_date, '%Y-%m-%d')
+        rec['adsb_time'] = datetime.strptime(adsb_time, '%H:%M:%S')
+        rec['inserted_at'] = inserted_at
 
-    # We take the combination of "now" and "hex" to be a compound key
     stmt = insert(Flights).values(data)
     stmt = stmt.on_conflict_do_nothing(index_elements=[Flights.now, Flights.hex])
 
@@ -61,6 +63,6 @@ with Session(engine) as session:
         session.execute(stmt)
         session.commit()
     except Exception as e:
-        logging.error(f'An error occurred during bulk insert: {e}')
+        logging.error('An error occurred during bulk insert: %s', str(e))
 
 print('Elapsed time on bulk insert: ', time.time() - start)
